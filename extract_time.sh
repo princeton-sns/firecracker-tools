28c4fba02894d1173dd6c401219624097da2feb9
LedgeDash <hl7@cs.princeton.edu>
2019-04-19 10:31:53 -0400

    logger output processing script to extract boot time

---
+++ extract_time.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#DATA_DIR=${1:-.}
+DATA_DIR="output"
+DEST="$PWD/data.log"
+
+rm -f $DEST
+
+pushd $DATA_DIR > /dev/null
+
+COUNT=`ls fc-sb* | sort -V | tail -1 | cut -d '-' -f 2 | cut -f 2 -d 'b'`
+
+for i in `seq 0 $COUNT`
+do
+    boot_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' | cut -f 4 -d ' '`
+    echo "$i boot $boot_time ms" >> $DEST
+done
+
+popd > /dev/null
