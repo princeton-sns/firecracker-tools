7a14168203fe3121c61dfcff5e755331c94ade09
LedgeDash <hl7@cs.princeton.edu>
2019-05-08 00:10:49 -0400

    extract kernel, init and python time

--- measurements/extract_time.sh
+++ measurements/extract_time.sh
@@ -3,6 +3,9 @@
 #DATA_DIR=${1:-.}
 DATA_DIR="output"
 DEST="$PWD/data.log"
+KERNEL="$PWD/kernel.log"
+INIT="$PWD/init.log"
+PY="$PWD/python.log"
 
 rm -f $DEST
 
@@ -12,8 +15,13 @@ COUNT=`ls fc-sb* | sort -V | tail -1 | cut -d '-' -f 2 | cut -f 2 -d 'b'`
 
 for i in `seq 0 $COUNT`
 do
-    boot_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' | cut -f 4 -d ' '`
-    echo "$i boot $boot_time ms" >> $DEST
+    kernel_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==1'`
+    init_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==2'`
+    python_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==3'`
+    #echo "$i boot $boot_time ms" >> $DEST
+    echo "$kernel_time" >> $KERNEL
+    echo "$init_time" >> $INIT
+    echo "$python_time" >> $PY
 done
 
 popd > /dev/null
