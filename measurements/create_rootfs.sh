9f690f7adf30b1283fcc1252c496ddc9571b50a9
LedgeDash <hl7@cs.princeton.edu>
2019-05-23 19:38:38 -0400

    script for building app specific rootfs from a base

---
+++ measurements/create_rootfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+base="${1}"
+target="${2}"
+app="${3}"
+
+echo "creating $target based on $base"
+sudo umount /tmp/my-rootfs
+cp $base $target
+mkdir -p /tmp/my-rootfs
+sudo mount $target /tmp/my-rootfs
+
+echo "copying app: $app"
+sudo cp workload/$app/app.alpine.zip /tmp/my-rootfs/srv/
+
+# Copying binary compiled on Ubuntu to a Alpine rootfs doesn't work
+#sudo cp ts /tmp/my-rootfs/srv/
+
