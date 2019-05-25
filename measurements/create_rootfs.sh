df32d128c305e764846bbf32426875c702430995
LedgeDash <hl7@cs.princeton.edu>
2019-05-24 21:33:31 -0400

    directly copy app's files instead of zip

--- measurements/create_rootfs.sh
+++ measurements/create_rootfs.sh
@@ -4,13 +4,14 @@ target="${2}"
 app="${3}"
 
 echo "creating $target based on $base"
+mkdir -p /tmp/my-rootfs
 sudo umount /tmp/my-rootfs
 cp $base $target
-mkdir -p /tmp/my-rootfs
 sudo mount $target /tmp/my-rootfs
 
 echo "copying app: $app"
-sudo cp workload/$app/app.alpine.zip /tmp/my-rootfs/srv/
+#sudo cp workload/$app/app.alpine.zip /tmp/my-rootfs/srv/
+sudo cp -r $PWD/$app/* /tmp/my-rootfs/srv
 
 # Copying binary compiled on Ubuntu to a Alpine rootfs doesn't work
 #sudo cp ts /tmp/my-rootfs/srv/
