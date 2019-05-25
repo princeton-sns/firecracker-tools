cd02b6d1ad3a381763ec65a830efc2e8c6163e0c
LedgeDash <hl7@cs.princeton.edu>
2019-05-25 12:20:32 -0400

    analysis with python initialization latency

--- measurements/extract_time.sh
+++ measurements/extract_time.sh
@@ -4,11 +4,12 @@
 DATA_DIR="output"
 KERNEL="$PWD/kernel.log"
 INIT="$PWD/init.log"
+PY="$PWD/python.log"
 IMPORT="$PWD/import.log"
 APP="$PWD/app.log"
 FC_SETUP="$PWD/fc_setup.log"
 
-rm -f $KERNEL $INIT $IMPORT $APP $FC_SETUP
+rm -f $KERNEL $INIT $IMPORT $APP $FC_SETUP $PY
 
 pushd $DATA_DIR > /dev/null
 
@@ -20,11 +21,13 @@ do
     fc_setup_time=`grep FC-guest fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ':' | cut -f 2 -d ' '`
     kernel_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==1'`
     init_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==2'`
-    import_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==3'`
-    app_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==4'`
+    python_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==3'`
+    import_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==4'`
+    app_time=`grep Guest-boot fc-log-${i} | cut -f 2 -d '=' |cut -f 2 -d ',' |  cut -f 5 -d ' ' | awk 'NR==5'`
 	echo "$fc_setup_time" >>$FC_SETUP
     echo "$kernel_time" >> $KERNEL
     echo "$init_time" >> $INIT
+	echo "$python_time" >> $PY
 	echo "$import_time" >> $IMPORT
     echo "$app_time" >> $APP
 done
