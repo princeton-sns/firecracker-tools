af2f37a9800ad4c2fd90d77e5c0015d9abd9a457
LedgeDash <hl7@cs.princeton.edu>
2019-05-24 21:34:53 -0400

    create rootfs for all python apps

--- measurements/create_workload_rootfs.sh
+++ measurements/create_workload_rootfs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-./create_rootfs.sh images/python-base.ext4 images/mb-json-python.ext4 microbenchmarks/json
-./create_rootfs.sh images/python-base.ext4 images/mb-base64-python.ext4 microbenchmarks/base64
-./create_rootfs.sh images/python-base.ext4 images/mb-primes-python.ext4 microbenchmarks/primes
-./create_rootfs.sh images/python-base.ext4 images/mb-http-python.ext4 microbenchmarks/http-endpoint
+BASE="${1}"
+PREFIX="${2}"
 
-./create_rootfs.sh images/python-base.ext4 images/markdown2html-python.ext4 markdown-to-html
+./create_rootfs.sh $BASE images/json-python.ext4 workload/microbenchmarks/json
+./create_rootfs.sh $BASE images/base64-python.ext4 workload/microbenchmarks/base64
+./create_rootfs.sh $BASE images/primes-python.ext4 workload/microbenchmarks/primes
+./create_rootfs.sh $BASE images/http-python.ext4 workload/microbenchmarks/http-endpoint
+
+./create_rootfs.sh $BASE images/markdown2html-python.ext4 workload/markdown-to-html
+./create_rootfs.sh $BASE images/sentiment-python.ext4 workload/sentiment-analysis
+
--- measurements/kill_all_fc.sh
+++ measurements/kill_all_fc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps aux | grep firecracker | tr -s ' ' | cut -d ' ' -f 2 | while read -r line; do
+ps aux | grep fire | tr -s ' ' | cut -d ' ' -f 2 | while read -r line; do
     echo $line
     sudo kill -9 $line
 done
