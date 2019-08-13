82af9adb21344794554ea40ddbc212a85ad64b13
Yue Tan <yuetan@cs.princeton.edu>
2019-08-12 22:16:22 -0400

    add C program outl.c under images/common which does outl assembly

--- images/prelude.sh
+++ images/prelude.sh
@@ -1,6 +1,7 @@
 apk add openrc util-linux
 
 cp /common/nc-vsock /usr/bin/nc-vsock
+cp /common/outl /usr/bin/outl
 
 ## Create start script for that mounts the appfs and invokes whatever binary is in /srv/workload
 printf '#!/bin/sh\n
