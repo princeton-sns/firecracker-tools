ebf07d9682fbdb6bdc4aa6304f5573cc996920a4
Yue Tan <yuetan@cs.princeton.edu>
2019-08-13 01:48:55 -0500

    connect to host vsock inside python2 script

--- images/runtimes/python2/workload.py
+++ images/runtimes/python2/workload.py
@@ -5,21 +5,22 @@ import os
 import imp
 import struct
 import json
+from subprocess import Popen, PIPE
 
+vsock = Popen('nc-vsock 0 1234', shell=True, stdout=PIPE, stdin=PIPE)
 os.system("mount -r /dev/vdb /srv");
 
 sys.path.append('/srv/package')
 app = imp.load_source('app', '/srv/workload')
 
 while True:
-    jsonlen = struct.unpack("@B", sys.stdin.read(1))[0]
-    request = json.loads(sys.stdin.read(jsonlen))
+    jsonlen = struct.unpack("@B", vsock.stdout.read(1))[0]
+    request = json.loads(vsock.stdout.read(jsonlen))
 
     response = app.handle(request)
 
     responseJson = json.dumps(response)
 
-    sys.stderr.write(struct.pack("@B", len(responseJson)))
-    sys.stderr.write(bytes(responseJson))
-    sys.stderr.flush()
-
+    vsock.stdin.write(struct.pack("@B", len(responseJson)))
+    vsock.stdin.write(bytes(responseJson))
+    vsock.stdin.flush()
