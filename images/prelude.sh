27f10894a94d3fcdfa7a62336f45b304e088f460
Yue Tan <yuetan@cs.princeton.edu>
2019-08-19 11:31:09 -0500

    updated firerunner README and discard stty output inside prelude.sh

--- images/prelude.sh
+++ images/prelude.sh
@@ -5,7 +5,7 @@ cp /common/outl /usr/bin/outl
 
 ## Create start script for that mounts the appfs and invokes whatever binary is in /srv/workload
 printf '#!/bin/sh\n
-stty -F /dev/ttyS1 -echo\n
+stty -F /dev/ttyS1 -echo >/dev/null\n
 exec /bin/runtime-workload >/dev/ttyS1\n' > /bin/workload
 chmod +x /bin/workload
 
