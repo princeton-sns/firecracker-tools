4ee3531bb3af0274a0d4df3cea20d74fe78aed86
Yue Tan <yuetan@cs.princeton.edu>
2019-08-23 02:43:28 -0500

    controller works with pipes now TODO: allow controller to boot functions from snapshots

--- firerunner/bins/controller/listener.rs
+++ firerunner/bins/controller/listener.rs
@@ -3,21 +3,26 @@ use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread::{self, JoinHandle};
-use firerunner::vsock::{self, VsockCloser, VsockListener, VsockStream};
+//use firerunner::vsock::{self, VsockCloser, VsockListener};
+use std::fs::File;
 
 use super::request;
+use firerunner::pipe_pair::PipePair;
 
 pub struct RequestManager {
-    listener: VsockListener,
-    channels: Arc<Mutex<BTreeMap<u32, (String, Receiver<request::Request>)>>>,
+    listener: File, // read end of the pipe through which VM signals it is ready to receive requests.
+    channels: Arc<Mutex<BTreeMap<u32, (String, Receiver<request::Request>, PipePair)>>>,
     connections: BTreeMap<u32, JoinHandle<()>>,
     response_sender: Sender<(String, Vec<u8>)>,
 }
 
 impl RequestManager {
-    pub fn new(channels: Arc<Mutex<BTreeMap<u32, (String, Receiver<request::Request>)>>>, response_sender: Sender<(String, Vec<u8>)>) -> RequestManager {
+    pub fn new(channels: Arc<Mutex<BTreeMap<u32, (String, Receiver<request::Request>, PipePair)>>>,
+                response_sender: Sender<(String, Vec<u8>)>,
+                listener: File) -> RequestManager
+    {
         RequestManager {
-            listener: VsockListener::bind(vsock::VMADDR_CID_ANY, 1234).expect("vsock listen"),
+            listener: listener,
             channels,
             connections: BTreeMap::new(),
             response_sender,
@@ -25,10 +30,13 @@ impl RequestManager {
     }
 
     pub fn serve(&mut self) {
-        while let Ok((connection, addr)) = self.listener.accept() {
-            if let Some((function, request_receiver)) = self.channels.lock().expect("poisoned lock").remove(&addr.cid) {
+        let mut id_buf = [0u8; 4]; // u32
+        loop {
+            self.listener.read_exact(&mut id_buf).expect("Failed to read from listener pipe");
+            let id = u32::from_le_bytes(id_buf);
+            if let Some((function, request_receiver, connection)) = self.channels.lock().expect("poisoned lock").remove(&id) {
                 let response_sender = self.response_sender.clone();
-                self.connections.insert(addr.cid, thread::spawn(move || {
+                self.connections.insert(id, thread::spawn(move || {
                     let mut conn_mgr = ConnectionManager {
                         function,
                         request_receiver,
@@ -39,27 +47,29 @@ impl RequestManager {
                 }));
             }
         }
-        println!("Done listening");
     }
 
-    pub fn spawn(mut self) -> (JoinHandle<()>, VsockCloser) {
-        let closer = self.listener.closer();
-        (thread::spawn(move || { self.serve() }), closer)
+    //pub fn spawn(mut self) -> (JoinHandle<()>, VsockCloser) {
+    //    let closer = self.listener.closer();
+    //    (thread::spawn(move || { self.serve() }), closer)
+    //}
+    pub fn spawn(mut self) -> JoinHandle<()> {
+        thread::spawn(move || { self.serve() })
     }
 }
 
-struct ConnectionManager {
+struct ConnectionManager<T>{
     function: String,
     request_receiver: Receiver<request::Request>,
     response_sender: Sender<(String, Vec<u8>)>,
-    connection: VsockStream,
+    connection: T,
 }
 
-impl ConnectionManager {
+impl<T: Read + Write> ConnectionManager<T> {
 
-    fn handle_request(connection: &mut VsockStream, request: request::Request) -> std::io::Result<Vec<u8>> {
-        let request = serde_json::to_vec(&request).unwrap();
-        connection.write_all(&[request.len() as u8])?;
+    fn handle_request(connection: &mut T, request: request::Request) -> std::io::Result<Vec<u8>> {
+        let mut request = serde_json::to_vec(&request).unwrap();
+        request.push(0xa); // newline
         connection.write_all(request.as_slice())?;
         let mut lens = [0];
         connection.read_exact(&mut lens)?;
