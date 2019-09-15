0123f0ccb9c5e5e6edf66b4eb8b76315fd3d1cf6
Amit Aryeh Levy <amit@amitlevy.com>
2019-09-15 09:59:30 -0400

    Geni profile wip

---
+++ profile.py
@@ -0,0 +1,34 @@
+"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`.
+This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
+Instructions:
+Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item.
+"""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+pc.defineParameter("hardware_type",
+                   "Optional physical node type (d710, c8220, etc)",
+                   portal.ParameterType.STRING, "")
+
+pc.bindParameter()
+
+# Add a raw PC to the request.
+node = request.RawPC("node")
+if params.hardware_type != "":
+    node.hardware_type = params.hardware_type
+    pass
+
+# Install and execute a script that is contained in the repository.
+#node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
